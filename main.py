d5cb8de3571d2e84b3ba2ea0170d42d5e5354bbe
tom <tmmunyiri@gmail.com>
2021-03-29 15:03:01 +0300

    edit fix

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ class form(QMainWindow):
         self.model.insertRows(self.i,1)
         self.model.setData(self.model.index(self.i,1),self.ui.lineEdit.text())
         self.model.setData(self.model.index(self.i, 2), self.ui.lineEdit_2.text())
-        self.model.setData(self.model.index(self.i,4), self.ui.lineEdit_3.text())
-        self.model.setData(self.model.index(self.i,3), self.ui.dateEdit.text())
+        self.model.setData(self.model.index(self.i,3), self.ui.lineEdit_3.text())
+        self.model.setData(self.model.index(self.i,4), self.ui.dateEdit.text())
         self.model.setData(self.model.index(self.i,5), self.ui.lineEdit_4.text())
         self.model.setData(self.model.index(self.i,6), self.ui.lineEdit_5.text())
         self.model.submitAll()
@@ -57,10 +57,12 @@ class form(QMainWindow):
     def updaterow(self):
         if self.ui.tableWidget.currentIndex().row() > -1:
             record = self.model.record(self.ui.tableWidget.currentIndex().row())
-            record.setValue("Name",self.ui.lineEdit.text())
-            record.setValue("Surname",self.ui.lineEdit_2.text())
-            record.setValue("DOB", self.ui.dateEdit.text())
-            record.setValue("Phone", self.ui.lineEdit_3.text())
+            record.setValue("Module Name",self.ui.lineEdit.text())
+            record.setValue("Staff ID",self.ui.lineEdit_2.text())
+            record.setValue("Module Code", self.ui.lineEdit_3.text())
+            record.setValue("Submission Date", self.ui.dateEdit.text())
+            record.setValue("Weighting", self.ui.lineEdit_4.text())
+            record.setValue("Component", self.ui.lineEdit_5.text())
             self.model.setRecord(self.ui.tableWidget.currentIndex().row(), record)
         else:
             QMessageBox.question(self,'Message', "Please select a row would you like to update", QMessageBox.Ok)
