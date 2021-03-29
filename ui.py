edaabe8ac80d3cb0593154b2ad8d12ee2f3909c5
tom <tmmunyiri@gmail.com>
2021-03-29 14:34:38 +0300

    initial commit

---
+++ ui.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'E:\uipython\pyqtex\pyqtdb\crud.ui'
+#
+# Created by: PyQt5 UI code generator 5.8.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(673, 620)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox.setGeometry(QtCore.QRect(0, 0, 671, 400))
+        self.groupBox.setObjectName("groupBox")
+        self.lcdNumber = QtWidgets.QLCDNumber(self.groupBox)
+        self.lcdNumber.setGeometry(QtCore.QRect(520, 10, 151, 81))
+        self.lcdNumber.setObjectName("lcdNumber")
+        
+        self.label = QtWidgets.QLabel(self.groupBox)
+        self.label.setGeometry(QtCore.QRect(10, 20, 87, 13))
+        self.label.setObjectName("label")
+        self.lineEdit = QtWidgets.QLineEdit(self.groupBox)
+        self.lineEdit.setGeometry(QtCore.QRect(10, 40, 411, 20))
+        self.lineEdit.setObjectName("lineEdit")
+
+        self.label_2 = QtWidgets.QLabel(self.groupBox)
+        self.label_2.setGeometry(QtCore.QRect(10, 70, 87, 13))
+        self.label_2.setObjectName("label_2")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.groupBox)
+        self.lineEdit_2.setGeometry(QtCore.QRect(10, 90, 411, 20))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+
+        self.label_3 = QtWidgets.QLabel(self.groupBox)
+        self.label_3.setGeometry(QtCore.QRect(10, 120, 97, 13))
+        self.label_3.setObjectName("label_3")
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.groupBox)
+        self.lineEdit_3.setGeometry(QtCore.QRect(10, 140, 411, 20))
+        self.lineEdit_3.setObjectName("lineEdit_3")
+
+        self.label_4 = QtWidgets.QLabel(self.groupBox)
+        self.label_4.setGeometry(QtCore.QRect(10, 170, 107, 13))
+        self.label_4.setObjectName("label_4")
+        self.dateEdit = QtWidgets.QDateEdit(self.groupBox)
+        self.dateEdit.setGeometry(QtCore.QRect(10, 190, 411, 22))
+        self.dateEdit.setObjectName("dateEdit")
+
+        self.label_5 = QtWidgets.QLabel(self.groupBox)
+        self.label_5.setGeometry(QtCore.QRect(10, 220, 97, 13))
+        self.label_5.setObjectName("label_5")
+        self.lineEdit_4 = QtWidgets.QLineEdit(self.groupBox)
+        self.lineEdit_4.setGeometry(QtCore.QRect(10, 240, 411, 20))
+        self.lineEdit_4.setObjectName("lineEdit_4")
+
+        self.label_6 = QtWidgets.QLabel(self.groupBox)
+        self.label_6.setGeometry(QtCore.QRect(10, 270, 97, 13))
+        self.label_6.setObjectName("label_6")
+        self.lineEdit_5 = QtWidgets.QLineEdit(self.groupBox)
+        self.lineEdit_5.setGeometry(QtCore.QRect(10, 290, 411, 20))
+        self.lineEdit_5.setObjectName("lineEdit_5")
+
+        self.tableWidget = QtWidgets.QTableView(self.centralwidget)
+        self.tableWidget.setGeometry(QtCore.QRect(0, 345, 671, 201))
+        self.tableWidget.setObjectName("tableWidget")
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea.setGeometry(QtCore.QRect(0, 550, 671, 41))
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 669, 39))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.pushButton = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        self.pushButton_2.setGeometry(QtCore.QRect(110, 10, 75, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        self.pushButton_3.setGeometry(QtCore.QRect(210, 10, 75, 23))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Student Assessment Database(SAD)"))
+        self.groupBox.setTitle(_translate("MainWindow", "Add Assessment"))
+        self.label.setText(_translate("MainWindow", "Module Name"))
+        self.label_2.setText(_translate("MainWindow", "Staff ID"))
+        self.label_3.setText(_translate("MainWindow", "Module Code"))
+        self.label_4.setText(_translate("MainWindow", "Submission Date"))
+        self.label_5.setText(_translate("MainWindow", "Weighting"))
+        self.label_6.setText(_translate("MainWindow", "Component"))
+        self.pushButton.setText(_translate("MainWindow", "Add"))
+        self.pushButton_2.setText(_translate("MainWindow", "Update"))
+        self.pushButton_3.setText(_translate("MainWindow", "Delete"))
+
